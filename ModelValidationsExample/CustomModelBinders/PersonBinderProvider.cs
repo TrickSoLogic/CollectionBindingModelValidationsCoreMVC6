6d5903015a7146bd2b577c3a10df57c1bf52e495
TrickSoLogic <118069084+TrickSoLogic@users.noreply.github.com>
2023-05-01 22:24:16 +0530

    Add project files.

---
+++ ModelValidationsExample/CustomModelBinders/PersonBinderProvider.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding;
+using Microsoft.AspNetCore.Mvc.ModelBinding.Binders;
+using CollectionBindingModelValidationsCoreMVC6.Models;
+
+namespace CollectionBindingModelValidationsCoreMVC6.CustomModelBinders
+{
+  public class PersonBinderProvider : IModelBinderProvider
+  {
+    public IModelBinder? GetBinder(ModelBinderProviderContext context)
+    {
+      if (context.Metadata.ModelType == typeof(Person))
+      {
+        return new BinderTypeModelBinder(typeof(PersonModelBinder));
+      }
+      return null;
+    }
+  }
+}
